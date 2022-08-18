e6715bbe3567f31c13f323ef09e1e46ef55e8082
blopezp09 <bryan.lopezp@sofka.com.co>
2022-08-18 16:21:18 -0500

    Correción de las dependencias en build.gradle

--- UI/src/test/java/co/com/sofka/runners/agregarregistro/AgregarUnNuevoRegistroExitoso.java
+++ UI/src/test/java/co/com/sofka/runners/agregarregistro/AgregarUnNuevoRegistroExitoso.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         features = {"src/test/resources/features/AgregarUnNuevoRegistro.feature"},
-        glue = {"co.com.sofka.stepdefinitions"}
+        glue = {"co.com.sofka.stepdefinitions.stepdefinitionagregarregistro"}
 )
 public class AgregarUnNuevoRegistroExitoso {
 }
--- UI/src/test/java/co/com/sofka/stepdefinitions/stepdefinitionagregarregistro/AgregarRegistroExitosoStepDefinition.java
+++ UI/src/test/java/co/com/sofka/stepdefinitions/stepdefinitionagregarregistro/AgregarRegistroExitosoStepDefinition.java
@@ -1,4 +1,21 @@
 package co.com.sofka.stepdefinitions.stepdefinitionagregarregistro;
 
+import io.cucumber.java.ast.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
 public class AgregarRegistroExitosoStepDefinition {
+
+    @Dado("que el usuario esté en la página y navegue hasta el apartado web tables")
+    public void queElUsuarioEsteEnLaPaginaYNavegueHastaElApartadoWebTables() {
+
+    }
+    @Cuando("el usuario suministra los campos solicitados")
+    public void elUsuarioSuministraLosCamposSolicitados() {
+
+    }
+    @Entonces("el sistema validará y agregará el registro exitosamente")
+    public void elSistemaValidaraYAgregaraElRegistroExitosamente() {
+
+    }
 }

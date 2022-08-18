0f0d9d3aef0669008f2867d55d3a6802338800f8
blopezp09 <bryan.lopezp@sofka.com.co>
2022-08-18 15:09:14 -0500

    Se agregó la clase runner para agregar un nuevo registro

---
+++ UI/src/test/java/co/com/sofka/runners/agregarregistro/AgregarUnNuevoRegistroExitoso.java
@@ -0,0 +1,14 @@
+package co.com.sofka.runners.agregarregistro;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        features = {"src/test/resources/features/AgregarUnNuevoRegistro.feature"},
+        glue = {"co.com.sofka.stepdefinitions"}
+)
+public class AgregarUnNuevoRegistroExitoso {
+}
---
+++ UI/src/test/java/co/com/sofka/stepdefinitions/stepdefinitionagregarregistro/AgregarRegistroExitosoStepDefinition.java
@@ -0,0 +1,4 @@
+package co.com.sofka.stepdefinitions.stepdefinitionagregarregistro;
+
+public class AgregarRegistroExitosoStepDefinition {
+}
